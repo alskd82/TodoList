a3999316cbf435ebd8b799d6c91bd37145247721
jungsun.yoo <alskd82@gmail.com>
2023-05-07 17:25:33 +0900

    uuid사용하기

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import {v4 as uuidv4} from "uuid";
+/*
+uuid 의 버전4를 사용하는데... uuidv4 라는 이름으로 사용할 것이다
+ */
 
 export default function AddTodo({ onAdd }) {
     const [text, setText] = useState("");
@@ -8,7 +12,7 @@ export default function AddTodo({ onAdd }) {
     const handleSubmit=(e)=>{
         e.preventDefault(); // 페이지가 새로고침 되는 것을 막는다.
         if(text.trim().length === 0) return; // 공백일 경우 추가하지 않는다.
-        onAdd({id: "고유한값", text, status: "active"});
+        onAdd({id: uuidv4(), text, status: "active"});
         setText(""); // input 초기화
     }
     return (
